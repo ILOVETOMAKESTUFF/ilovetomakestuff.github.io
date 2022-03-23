cc10c7cec3b26d64687ddc7e27cee7e6529d9e20
I_L_T_M_S <97681427+ILOVETOMAKESTUFF@users.noreply.github.com>
2022-03-23 13:21:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+const root = document.documentElement 
+const themeBtns = document.querySelectorAll('.theme > button')
+
+themeBtns.forEach((btn) => {
+  btn.addEventListener('click', handleThemeUpdate)
+})
+
+function handleThemeUpdate(e) {
+  switch(e.target.value) {
+    case 'Dark': 
+      root.style.setProperty('--bg', 'black')
+      root.style.setProperty('--bg-text', 'white')
+      break
+      
+    case 'Light':
+      root.style.setProperty('--bg', 'white')
+      root.style.setProperty('--bg-text', 'black')
+      break
+  }
+}
