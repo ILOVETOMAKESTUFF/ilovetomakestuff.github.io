83d579887d9ec7ea2f5708cdfe37a8c0616a384d
I_L_T_M_S <97681427+ILOVETOMAKESTUFF@users.noreply.github.com>
2022-04-11 18:28:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ setInterval(async() => {
   const urlToFetch = "https://scratch.mit.edu/users/" + user + "/followers?noCache=" + Math.random().toString();
   const followersPage = parser.parseFromString(await(await fetch(urlToFetch)).text(), 'text/html');
   const followerCount = followersPage.querySelector(".box-head h2").innerText.match(/\((.*)\)/)[1];
-  document.getElementById("counter").innerText = followerCount;
+  document.getElementById("counter").innerText = "Followers: " + followerCount;
 }, 5000)
 
 //Just experimenting!
 document.getElementById("date").innerHTML("The date is " + Date());
 
 function alertSaying(e) {
-  window.alert("bruh, " + e);
+  alert("bruh, " + e);
 } 
