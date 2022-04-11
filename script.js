45083d5a284d65960df89d285d19e015da95c1d1
I_L_T_M_S <97681427+ILOVETOMAKESTUFF@users.noreply.github.com>
2022-04-11 17:40:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+var user = "I_LOVE_TO_MAKE_STUFF";
+const parser = new window.DOMParser();
+setInterval(async() => {
+  const urlToFetch = "https://scratch.mit.edu/users/" + user + "/followers?noCache=" + Math.random().toString();
+  const followersPage = parser.parseFromString(await(await fetch(urlToFetch)).text(), 'text/html');
+  const followerCount = followersPage.querySelector(".box-head h2").innerText.match(/\((.*)\)/)[1];
+  document.getElementById("counter").innerText = followerCount;
+}, 5000)
