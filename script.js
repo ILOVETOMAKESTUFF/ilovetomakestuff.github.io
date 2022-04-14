7ef901dec5a999365a6bd56d3906de3bc6dd9b70
I_L_T_M_S <97681427+ILOVETOMAKESTUFF@users.noreply.github.com>
2022-04-14 13:49:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Startup
 let theme = "light";
-function get(query) {return document.querySelector(query)}; // Taken from lopste/drawing-station, check it out!
+
 // These two functions were from ATs
 function getFollowers() {
   var user = "ISTILLMAKESTUFF";
@@ -9,7 +9,7 @@ function getFollowers() {
     const urlToFetch = "https://scratch.mit.edu/users/" + user + "/followers?noCache=" + Math.random().toString();
     const followersPage = parser.parseFromString(await(await fetch(urlToFetch)).text(), 'text/html');
     const followerCount = followersPage.querySelector(".box-head h2").innerText.match(/\((.*)\)/)[1];
-    get("counter").innerText = "Followers: " + followerCount;
+    document.getElementById("counter").innerText = "Followers: " + followerCount;
   }, 5000)
 }
 
@@ -17,7 +17,7 @@ function getMessages() {
   fetch("https://api.scratch.mit.edu/users/ISTILLMAKESTUFF/messages/count")
   .then(res => res.json())
   .then(res => {
-   get("countermessage").innerText = "Message count: " + res.count;
+   document.getElementById("countermessage").innerText = "Message count: " + res.count;
 })
 }
 
@@ -25,12 +25,12 @@ function getMessages() {
 document.getElementById("date").innerHTML("The date is " + Date()); */
 
 function switchThemes() {
-  theme = get("body").className;
+  theme = document.getElementsByTagName("body").className;
   if (theme == "light") {
-    get("body").className = dark;
-    get("themebutton").innerText = "Light";
+    document.getElementsByTagName("body").className = dark;
+    document.getElementById("themebutton").innerText = "Light";
   } else if (theme == "dark") {
-    get("body").className = "light";
-    get("themebutton").innerText = "Dark";
+    document.getElementsByTagName("body").className = "light";
+    document.getElementById("themebutton").innerHTML = "Dark";
   }
 }
